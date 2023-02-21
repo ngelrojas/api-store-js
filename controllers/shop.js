bf95c95368a26f52955d2dfcc4836046f222878c
ngel rojas <aomarrojasp@gmail.com>
2023-02-20 21:02:29 -0300

    add: adding update products

--- controllers/shop.js
+++ controllers/shop.js
@@ -33,6 +33,24 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
+  Cart.getCart(cart => {
+    Product.fetchAll(products => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cart.products.find(
+          prod => prod.id === product.id
+        );
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: cartProducts
+      });
+    });
+  })
   res.render('shop/cart', {
     path: '/cart',
     pageTitle: 'Your Cart'
@@ -46,6 +64,14 @@ exports.postCart = (req, res, next) => {
   });
   res.redirect('/cart');
 };
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect('/cart');
+  })
+}
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
