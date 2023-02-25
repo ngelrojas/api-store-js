098b8394b44d5b98f122e66142f3c8febc06b03b
ngel rojas <aomarrojasp@gmail.com>
2023-02-25 20:51:42 -0300

    add: install mongoose and create string connections

--- util/database.js
+++ util/database.js
@@ -1,8 +1,17 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = callback => {
 
-module.exports = sequelize;
+  MongoClient.connect(
+    'mongodb://root:root@localhost:27017/',
+    { useNewUrlParser: true },
+  ).then(result => {
+    console.log('Connected!');
+    callback(result);
+  }).catch(err => {
+    console.log(err);
+  })
+}
+
+module.exports = mongoConnect;
